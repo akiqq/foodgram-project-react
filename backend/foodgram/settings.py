ee5bc6350273cf625c84d41ff62281a55261c778
Alexandr Ainullin <aainullin@gmail.com>
2023-04-07 23:30:46 +0300

    rev.7

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='mysecretkeykslhtyn^##a1)ilz@4zqj=r
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['178.154.192.242', 'backend', 'localhost']
+ALLOWED_HOSTS = ['178.154.192.242', 'backend']
 
 
 INSTALLED_APPS = [
